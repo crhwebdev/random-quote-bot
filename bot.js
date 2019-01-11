0c5b717d418781a45384df597f21b120d703ee8a
Carl Hain <crhain@gmail.com>
2019-01-11 10:41:54 -0600

    Add random quote reply to bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,14 @@
 const Discord = require('discord.js');
 const keys = require('./config/keys');
+const quotes = require('./quotes/quote');
 const client = new Discord.Client();
+
+const getRandomQuote = quoteArray => {
+  let randomIndex = Math.floor(
+    Math.random() * Math.floor(quoteArray.length - 1)
+  );
+  return quoteArray[randomIndex];
+};
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -8,7 +16,7 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === '!ping') {
-    msg.reply('Pong!');
+    msg.reply(getRandomQuote(quotes));
   }
 });
 
