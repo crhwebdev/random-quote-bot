04b85efee2870e468e32c6fb9c319207dda9e89b
Carl Hain <crhain@gmail.com>
2019-01-11 10:04:33 -0600

    Remove discord.io and install discord.js instead; rewrite bot.js to use discord.js (and actually work)

--- bot.js
+++ bot.js
@@ -1,42 +1,15 @@
-const Discord = require('discord.io');
-const logger = require('winston');
+const Discord = require('discord.js');
 const keys = require('./config/keys');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-  colorize: true
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-logger.level = 'debug';
-// Initialize Discord Bot
-const bot = new Discord.Client({
-  token: keys.discordToken,
-  autorun: true
-});
-
-bot.on('ready', evt => {
-  logger.info('Connected');
-  logger.info('Logged in as: ');
-  logger.info(bot.username + ' - (' + bot.id + ')');
-});
-
-bot.on('message', (user, userID, channelID, message, evt) => {
-  // Our bot needs to know if it will execute a command
-  // It will listen for messages that will start with `!`
-  if (message.substring(0, 1) == '!') {
-    let args = message.substring(1).split(' ');
-    let cmd = args[0];
-
-    args = args.splice(1);
-    switch (cmd) {
-      // !ping
-      case 'ping':
-        bot.sendMessage({
-          to: channelID,
-          message: 'Pong!'
-        });
-        break;
-      // Just add any case commands if you want to..
-    }
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
   }
 });
+
+client.login(keys.discordToken);
