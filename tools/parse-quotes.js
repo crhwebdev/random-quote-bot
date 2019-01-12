b1938fb782c78ea3135d75795ea3e351e7d9daee
Carl Hain <crhain@gmail.com>
2019-01-12 09:04:13 -0600

    Add tools directory and new file for parse-quote.js tool that parses quotes from a text file into JSON format - tool not implimented yet

---
+++ tools/parse-quotes.js
@@ -0,0 +1,5 @@
+/*
+  useage: node parse-quotes [input filename] [output filename]
+  description: breaks file into lines using endofline indicator, places quote in a list, and then when finished parsing list, it
+  converts the list to JSON and exports it to new file (or appends it if file already has contents)
+*/
