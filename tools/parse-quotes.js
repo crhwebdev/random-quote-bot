add1a4cd07f6f7a0b39dacfe1b10cc1d17c97a70
Carl Hain <crhain@gmail.com>
2019-01-12 13:25:04 -0600

    Refactor parse-quotes.js to make it more readable; add defaults for running parse-quotes.js without arguments

--- tools/parse-quotes.js
+++ tools/parse-quotes.js
@@ -3,11 +3,9 @@
   description: breaks file into lines using endofline indicator, places quote in a list, and then when finished parsing list, it
   converts the list to JSON and exports it to new file (or appends it if file already has contents)
 */
-const readline = require('readline');
 const fs = require('fs');
-const inputFile = process.argv[2] || '/data/input.txt';
-const outputFile = process.argv[3] || '/data/output.json';
-let inputFileLines = [];
+const path = require('path');
+const readline = require('readline');
 
 const dedupeLines = function(lines) {
   let unique = [...new Set(lines)];
@@ -25,36 +23,43 @@ const writeLinesToOutput = function(lines, output) {
   });
 };
 
-//create readline interface
-let rl = readline.createInterface({
-  input: fs.createReadStream(inputFile)
-});
-
-//read each line into lines list
-let line_no = 0;
-rl.on('line', line => {
-  line_no++;
-  inputFileLines.push(line);
-});
-
-//write lines to output file once read stream closes
-rl.on('close', line => {
-  //check to see if outputFile exists
-  fs.stat(outputFile, (err, stats) => {
-    //if it does not exist, then just write inputFileLines to outputFile
-    if (err) {
-      console.log('Output file does not exist. Creating file....');
-      writeLinesToOutput(inputFileLines, outputFile);
-      //if it does exist, then read in files from existing outputFile
-      // parse them to an array, concat new outputFileLines with inputFileLines
-      // and then write combined array to file
-    } else {
-      console.log('Output file exists.');
-      fs.readFile(outputFile, 'utf8', (err, contents) => {
-        let outputFileLines = JSON.parse(contents);
-        let combinedFileLines = outputFileLines.concat(inputFileLines);
-        writeLinesToOutput(combinedFileLines, outputFile);
-      });
-    }
+const parseQuotes = function(inputFile, outputFile) {
+  //create readline interface
+  let rl = readline.createInterface({
+    input: fs.createReadStream(inputFile)
   });
-});
+
+  //read each line from input file into lines list
+  let inputFileLines = [];
+  let line_no = 0;
+  rl.on('line', line => {
+    line_no++;
+    inputFileLines.push(line);
+  });
+
+  //write input file lines to output file once read stream closes
+  rl.on('close', line => {
+    //check to see if outputFile exists
+    fs.stat(outputFile, (err, stats) => {
+      //if it does not exist, then just write inputFileLines to outputFile
+      if (err) {
+        console.log('Output file does not exist. Creating file....');
+        writeLinesToOutput(inputFileLines, outputFile);
+        //if it does exist, then read in files from existing outputFile
+        // parse them to an array, concat new outputFileLines with inputFileLines
+        // and then write combined array to file
+      } else {
+        console.log('Output file exists.');
+        fs.readFile(outputFile, 'utf8', (err, contents) => {
+          let outputFileLines = JSON.parse(contents);
+          let combinedFileLines = outputFileLines.concat(inputFileLines);
+          writeLinesToOutput(combinedFileLines, outputFile);
+        });
+      }
+    });
+  });
+};
+
+const input = process.argv[2] || path.join(__dirname, '/data/input.txt');
+const output = process.argv[3] || path.join(__dirname, '/data/output.json');
+parseQuotes(input, output);
